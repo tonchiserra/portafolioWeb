ed5d350c52d403500ea88cf15e20eb71a14a9ff2
tonchiserra <tonchiserra@gmail.com>
2021-03-09 00:12:03 -0300

    Starting v3.0

--- js/script.js
+++
@@ -1,5 +0,0 @@
-function slideMenu(){
-    
-    document.getElementById("navMain").classList.toggle('moveNav');
-    document.getElementById("containerAll").classList.toggle('moveAll');
-}
---
+++ script.js
@@ -0,0 +1,19 @@
+//menu
+((d) => {
+    const $btnMenu = d.querySelector(".menu-btn")
+    const $menu = d.querySelector(".menu")
+
+    $btnMenu.addEventListener("click", (e) => {
+        $btnMenu.firstElementChild.classList.toggle("none")
+        $btnMenu.lastElementChild.classList.toggle("none")
+        $menu.classList.toggle("is-active")
+    })
+
+    d.addEventListener("click", (e) => {
+        if(!e.target.matches(".menu a")) return false
+
+        $btnMenu.firstElementChild.classList.remove("none")
+        $btnMenu.lastElementChild.classList.add("none")
+        $menu.classList.remove("is-active")
+    })
+})(document)
