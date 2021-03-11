5b63475aeda24e1106b19ffcf0a4efaa8969c405
tonchiserra <tonchiserra@gmail.com>
2021-03-11 00:26:41 -0300

    V3.0 Complete

--- script.js
+++ script.js
@@ -17,3 +17,39 @@
         $menu.classList.remove("is-active")
     })
 })(document)
+
+
+/* ------ Contacto ------*/
+function sendMail(){
+    const $form = document.querySelector(".contact-form");
+    const $loader = document.querySelector(".contact-loader");
+    const $modal = document.querySelector(".modal");
+    const $response = document.querySelector(".modal-contact-response");
+
+    $form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        $modal.classList.remove("modal-none");
+        $response.classList.add("modal-none");
+        $loader.classList.remove("modal-none");
+        
+        fetch("https://formsubmit.co/ajax/serragonzalo1@gmail.com", {
+            method: "POST",
+            body: new FormData(e.target)
+        })
+        .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+        .then((json) => {
+            $form.reset();
+        })
+        .catch((err) => {
+            let message = err.statusText || "Ocurrio un error, instenta nuevamente";
+            $response.querySelector("h3").innerHTML = `Error ${err.status}: ${message}`;
+        })
+        .finally(() => {
+            $loader.classList.add("modal-none");
+            $response.classList.remove("modal-none");
+            setTimeout(() => {
+                $modal.classList.add("modal-none");
+            }, 3000);
+        });
+    });
+};
